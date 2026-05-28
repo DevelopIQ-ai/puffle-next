dd59ab237848860393448a7be5e3844490a2ad2b
Kush Bhuwalka <kushbhuwalka@gmail.com>
2026-05-27 19:24:00 -0700

    Update homepage hero styling

--- app/HeroVisual.tsx
+++ app/HeroVisual.tsx
@@ -6,8 +6,8 @@ export default function HeroVisual() {
   return (
     <div className="hero-visual-container">
       <AsciiPortrait
-        src="/portrait.png"
-        cols={80}
+        src="/puffle-logo.svg"
+        cols={96}
         color="25, 50, 120"
         intensity={1.4}
       />
