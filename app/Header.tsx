5b558bb05f941f0bbeba5e01ac4a40a75c97abce
Kush Bhuwalka <kushbhuwalka@gmail.com>
2026-07-18 12:09:15 -0700

    style: simplify site header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { CONTACT_EMAIL, PRIMARY_NAVIGATION, WAITLIST_URL } from "./site";
+import { CONTACT_EMAIL, WAITLIST_URL } from "./site";
 
 export default function Header() {
   return (
@@ -22,15 +22,6 @@ export default function Header() {
           Puffle
         </Link>
         <div className="header-actions">
-          <nav aria-label="Primary navigation">
-            <ul>
-              {PRIMARY_NAVIGATION.map((item) => (
-                <li key={item.href}>
-                  <Link href={item.href}>{item.label}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
           <div className="nav-cta">
             <a href={`mailto:${CONTACT_EMAIL}`} className="top-nav-link header-contact-link">
               Contact
